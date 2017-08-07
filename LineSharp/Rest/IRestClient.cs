93a70d8404ebff676fb6a4bdb0b8130fbe193015
Masayuki Muto <masa.edw@gmail.com>
2017-08-07 17:38:19 +0900

    Introduce IRestClient

---
+++ LineSharp/Rest/IRestClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace LineSharp.Rest
+{
+    public interface IRestClient
+    {
+        Task PostAsync<TMessage>(string url, TMessage msg);
+
+        Task<TResponse> GetAsync<TResponse>(string url);
+
+        Task<byte[]> GetAsyncAsByteArray(string url);
+    }
+}
---
+++ LineSharp/Rest/RestClient.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using LineSharp.Messages;
+using Newtonsoft.Json;
+
+namespace LineSharp.Rest
+{
+    public class RestClient : IRestClient
+    {
+        public List<DelegatingHandler> HttpHandlers { get; } = new List<DelegatingHandler>();
+        public JsonMediaTypeFormatter Formatter { get; }
+        public string UrlPrefix { get; }
+        public string ChannelAccessToken { get; }
+
+        public RestClient(string urlPrefix, string channelAccessToken)
+        {
+            Formatter = new JsonMediaTypeFormatter();
+            Formatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            UrlPrefix = urlPrefix;
+            ChannelAccessToken = channelAccessToken;
+        }
+
+        public async Task PostAsync<TMessage>(string url, TMessage msg)
+        {
+            using (var client = HttpClientFactory.Create(HttpHandlers.ToArray()))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ChannelAccessToken);
+                var res = await client.PostAsync($"{UrlPrefix}{url}", msg, Formatter).ConfigureAwait(false);
+                if (!res.IsSuccessStatusCode)
+                {
+                    var body = await res.Content.ReadAsAsync<ErrorResponse>().ConfigureAwait(false);
+                    throw new LineException(body.Message, body);
+                }
+            }
+        }
+
+        public async Task<TResponse> GetAsync<TResponse>(string url)
+        {
+            using (var client = HttpClientFactory.Create(HttpHandlers.ToArray()))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ChannelAccessToken);
+                var res = await client.GetAsync($"{UrlPrefix}{url}").ConfigureAwait(false);
+                if (!res.IsSuccessStatusCode)
+                {
+                    var body = await res.Content.ReadAsAsync<ErrorResponse>();
+                    throw new LineException(body.Message, body);
+                }
+
+                return JsonConvert.DeserializeObject<TResponse>(await res.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+        }
+
+        public async Task<byte[]> GetAsyncAsByteArray(string url)
+        {
+            using (var client = HttpClientFactory.Create(HttpHandlers.ToArray()))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ChannelAccessToken);
+                var res = await client.GetAsync($"{UrlPrefix}{url}").ConfigureAwait(false);
+                if (!res.IsSuccessStatusCode)
+                {
+                    var body = await res.Content.ReadAsAsync<ErrorResponse>();
+                    throw new LineException(body.Message, body);
+                }
+
+                return await res.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
